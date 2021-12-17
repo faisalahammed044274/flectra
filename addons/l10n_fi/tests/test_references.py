c3db21e6d0f0f0555136e1661748be104f496d23
Parthiv Patel <parthiv@techreceptives.com>
2021-12-17 09:35:41 +0000

    [PATCH] Upstream patch - 17122021

--- addons/l10n_fi/tests/test_references.py
+++ addons/l10n_fi/tests/test_references.py
@@ -1,4 +1,4 @@
-# Part of Odoo, Flectra. See LICENSE file for full copyright and licensing details.
+# Part of Flectra. See LICENSE file for full copyright and licensing details.
 from flectra.tests import tagged
 from flectra.addons.account.tests.common import AccountTestInvoicingCommon
 from flectra.exceptions import UserError
